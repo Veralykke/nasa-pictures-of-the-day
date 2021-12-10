3845aed3bb89322fef32d4789c806549d34eb68c
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2021-12-10 11:49:07 +0100

    navbar and styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 /////////////
 //NB: Here im using the npm react-router-dom@5.3.0
+///// "Kapehe" on youtube
 ////////////
 
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NasaPhoto from "./components/NasaPhoto";
-import NavBar from "./components/NavBar";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="app">
         <Route component={Home} path="/" exact />
         <Route component={NasaPhoto} path="/nasaphoto" />
       </div>
--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import NavBar from "./NavBar";
+
+const apiKey = process.env.REACT_APP_NASA_KEY;
 
 export default function NasaPhoto() {
   const [photoData, setPhotoData] = useState(null);
@@ -9,7 +12,7 @@ export default function NasaPhoto() {
 
     async function fetchPhoto() {
       const res = await fetch(
-        `https://api.nasa.gov/planetary/apod?api_key=m8fkg2biKWIloC6Zg53cVks7zIoU0nWEQs3f7Ky1`
+        `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
       );
       const data = await res.json();
 
@@ -21,25 +24,28 @@ export default function NasaPhoto() {
   if (!photoData) return <div />;
 
   return (
-    <div>
-      {photoData.media_type === "image" ? (
-      <img src={photoData.url} alt={photoData.title} />
-      ) : (
+    <>
+      <NavBar />
+      <div className="nasa-photo">
+        {photoData.media_type === "image" ? (
+          <img src={photoData.url} alt={photoData.title} className="photo" />
+        ) : (
           <iframe
-          title="space-video"
-          src={photoData.url}
-          frameBorder="0"
-          gesture="media"
-          allow="encrypte-media"
-          allowFullScreen
-          className="photo"
+            title="space-video"
+            src={photoData.url}
+            frameBorder="0"
+            gesture="media"
+            allow="encrypte-media"
+            allowFullScreen
+            className="photo"
           />
-      )}
-      <div>
-        <h1>{photoData.title}</h1>
-        <p>{photoData.date}</p>
-        <p>{photoData.explanation}</p>
+        )}
+        <div>
+          <h1>{photoData.title}</h1>
+          <p className="date">{photoData.date}</p>
+          <p className="explanation">{photoData.explanation}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
